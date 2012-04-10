00ac0105c14f9907bef5e47d986fba820728ccbf
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-04-10 17:05:15 +0900

    Update build scripts

--- openshortcuts.bat
+++ openshortcuts.bat
@@ -1,5 +1,5 @@
 set appname=%~n0
 
-copy buildscript\make_new.sh .\
-bash make_new.sh %appname% version=1
-del make_new.sh
+copy buildscript\makexpi.sh .\
+bash makexpi.sh -n %appname% -v
+del makexpi.sh
--- openshortcuts.sh
+++ openshortcuts.sh
@@ -3,6 +3,6 @@
 appname=${0##*/}
 appname=${appname%.sh}
 
-cp buildscript/make_new.sh ./
-./make_new.sh $appname version=1
-rm ./make_new.sh
+cp buildscript/makexpi.sh ./
+./makexpi.sh -n $appname -v
+rm ./makexpi.sh
